4317344c915f153165a76eaba7e2593619a44d9f
Maksim Kalasouski <66016998+maxim-kolosovskii@users.noreply.github.com>
2022-07-22 21:37:05 +0300

    forms loader and checkbox

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+// onClick open loader and close it after time
+function envoiEnCours() {
+    var element = document.getElementById("form-blur");
+    element.classList.add("js-blur");
+    setTimeout(function() {element.classList.remove("js-blur")}, 2000);
+}
+
+
